9c15847dc27b1ca2050c9ec519b6b4eb0983add0
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-05-12 15:12:22 +0800

    feat: add motion

--- src/app/components/about-me-section.tsx
+++ src/app/components/about-me-section.tsx
@@ -2,18 +2,33 @@
 
 import { Glow, GlowCapture } from "@codaworks/react-glow";
 import NowPlayingWidget from "./now-playing-widget";
+import { motion } from "framer-motion";
+import SectionTitleMotion from "./motions/section-title-motion";
 
 export default function AboutMeSection() {
   return (
-    <section className="w-full grid place-content-center py-[7rem] relative">
+    <section
+      id="about"
+      className="w-full grid place-content-center py-[7rem] relative"
+    >
       <GlowCapture>
         <Glow>
-          <div className="bg-cardBg/80 w-full max-w-[1200px] relative rounded-2xl overflow-hidden border border-[#484848]/40  glow:bg-white/10">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.2, delay: 0.2 }}
+            className="bg-cardBg/80 w-full max-w-[1200px] relative rounded-2xl overflow-hidden border border-[#484848]/40  glow:bg-white/10"
+          >
             <div className="p-8 grid gap-6 md:max-w-[70%]">
-              <h2 className="text-headerText font-bold text-[32px] md:text-5xl">
-                About Me
-              </h2>
-              <div className="grid gap-4 z-10">
+              <SectionTitleMotion>About Me</SectionTitleMotion>
+              <motion.div
+                initial={{ y: 50, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.2, delay: 0.6 }}
+                className="grid gap-4 z-10"
+              >
                 <p className="text-textMuted text-lg leading-relaxed">
                   Hello, again. I am Alessandro Benig, 21 years old. Currently,
                   I&apos;m in my third year of taking a Bachelor of Science in
@@ -26,9 +41,9 @@ export default function AboutMeSection() {
                 <p className="text-textMuted text-lg leading-relaxed">
                   Up to this day, I&apos;ve been constantly learning
                   technologies and creating projects to enhance my skills and
-                  keep myself relevant in the industry.
+                  stay relevant in the industry.
                 </p>
-              </div>
+              </motion.div>
             </div>
             {/* nested circles */}
             <div className="rounded-full w-[400px] h-[400px] absolute -right-[7rem] -bottom-[7rem] bg-[#413f3f13] grid place-content-center">
@@ -36,7 +51,7 @@ export default function AboutMeSection() {
                 <div className="rounded-full w-[200px] h-[200px] bg-[#413f3f89]" />
               </div>
             </div>
-          </div>
+          </motion.div>
         </Glow>
       </GlowCapture>
     </section>
