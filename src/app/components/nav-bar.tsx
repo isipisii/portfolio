8b8187560ed29f9af25c7ce9647e78e8f5e9d944
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-05-12 15:12:59 +0800

    add motion

--- src/app/components/nav-bar.tsx
+++ src/app/components/nav-bar.tsx
@@ -1,11 +1,55 @@
+"use client";
+
 import NavLogo from "../assets/nav-logo.svg";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function Navbar() {
+
   return (
     <header className="fixed w-full backdrop-blur bg-[#151418]/20 z-20 flex justify-center">
-      <nav className="px-8 py-4 max-w-[1400px] w-full">
-        <Image src={NavLogo} width={40} height={40} alt="logo" />
+      <nav className="px-8 flex justify-between items-center py-4 max-w-[1400px] w-full">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.2 }}
+        >
+          <Image src={NavLogo} width={40} height={40} alt="logo" />
+        </motion.div>
+
+        <div className="flex gap-12">
+          <motion.a
+            initial={{ y: -20, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.2, delay: 0.4 }}
+            href="#about"
+            className="text-textMuted hover:text-primary/80"
+          >
+            About
+          </motion.a>
+          <motion.a
+            initial={{ y: -20, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.2, delay: 0.6 }}
+            href="#projects"
+            className="text-textMuted hover:text-primary/80"
+          >
+            Projects
+          </motion.a>
+          <motion.a
+            className="text-textMuted hover:text-primary/80"
+            initial={{ y: -20, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.2, delay: 0.8 }}
+            href="#contact"
+          >
+            Contact
+          </motion.a>
+        </div>
       </nav>
     </header>
   );
