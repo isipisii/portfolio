0397109bdfe3568c616e8317303903f8e1edfcf6
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-05-14 18:48:32 +0800

    remove repost star request due to limited  api rate

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -4,17 +4,19 @@ import { FaRegStar } from "react-icons/fa6";
 import { SiGithub } from "react-icons/si";
 import { FaLink } from "react-icons/fa6";
 
-export async function getRepoStars(repoName: string) {
-  const res = await fetch(`https://api.github.com/repos/isipisii/${repoName}`);
-  const data = await res.json();
-  return data.stargazers_count as number;
-}
+// the api rate limit exceeded
+// export async function getRepoStars(repoName: string) {
+//   const res = await fetch(`https://api.github.com/repos/isipisii/${repoName}`);
+//   const data = await res.json();
+//   console.log(data)
+//   return data.stargazers_count as number;
+// }
 
 export default async function ProjectCard({ project }: { project: TProject }) {
-  const stars = await getRepoStars(project.repoName);
-
+  // const stars = await getRepoStars(project.repoName);
+  
   return (
-    <div className=" w-full rounded-2xl gap-6 flex md:flex-row flex-col items-start relative overflow-hidden p-4">
+    <div className="w-full rounded-2xl gap-6 flex md:flex-row flex-col items-start relative overflow-hidden p-4">
       <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#484848,transparent_1px),linear-gradient(to_bottom,#484848,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_60%_at_60%_0%,#000_80%,transparent_110%)]" />
       <Image
         src={project.src}
@@ -53,10 +55,10 @@ export default async function ProjectCard({ project }: { project: TProject }) {
             </p>
           ))}
         </div>
-        <div className="flex gap-2 place-self-end">
+        {/* <div className="flex gap-2 place-self-end">
           <FaRegStar className="h-5 w-5 text-textMuted" />
           <p className="text-textMuted font-semibold">{stars}</p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
