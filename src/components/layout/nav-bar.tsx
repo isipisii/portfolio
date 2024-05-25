57f79e64762dd59f19f8627f602f52b932b210de
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-05-25 22:18:02 +0800

    add a listener for showing the bg and fix the image flickering

--- src/components/layout/nav-bar.tsx
+++ src/components/layout/nav-bar.tsx
@@ -5,25 +5,52 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import MobileMenu from "./mobile-menu";
 import { navItems } from "@/constants";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function Navbar() {
+  const [showBg, setShowBg] = useState(false);
 
   function handleScrollToTop() {
     window.scrollTo({ top: 0 });
   }
 
+  useEffect(() => {
+    function handleScrollY() {
+      if (window.scrollY > 100) {
+        setShowBg(true);
+      } else setShowBg(false);
+    }
+
+    document.addEventListener("scroll", handleScrollY);
+
+    return () => {
+      document.removeEventListener("scroll", handleScrollY);
+    };
+  }, []);
+
   return (
-    <header className="fixed w-full backdrop-blur bg-[#151418]/70 z-20 flex justify-center">
+    <header
+      className={twMerge(
+        "fixed w-full  z-20 flex justify-center transition duration-100 ease-in-out",
+        showBg ? "bg-[#151418]/70 backdrop-blur" : null
+      )}
+    >
       <nav className="px-8 flex justify-between items-center py-4 max-w-[1400px] w-full">
         <motion.div
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true }}
           transition={{ duration: 0.2, delay: 0.3 }}
-          onClick={handleScrollToTop}
-          className="cursor-pointer active:scale-90 transition ease-in-out duration-100"
         >
-          <Image src={NavLogo} width={60} height={60} alt="ale-logo" />
+          <Image
+            src={NavLogo}
+            width={60}
+            height={60}
+            alt="ale-logo"
+            onClick={handleScrollToTop}
+            className="cursor-pointer active:scale-90 transition ease-in-out duration-100"
+          />
         </motion.div>
 
         {/* for large viewports */}
@@ -43,7 +70,6 @@ export default function Navbar() {
           ))}
         </div>
 
-        
         {/* for small viewports */}
         <MobileMenu />
       </nav>
