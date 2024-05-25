dac67ed7392d12b59c3ddfbb566568f612025505
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-05-25 22:18:40 +0800

    add socials

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from "next";
 import { Jost } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/nav-bar";
+import Socials from "@/components/layout/socials";
 
 const jost = Jost({ subsets: ["latin"] });
-const TITLE = "Alessandro Benig"
-const DESCRIPTION = "A goal-driven and passionate student web developer based in the Philippines, solely focused on creating react applications."
+const TITLE = "Alessandro Benig";
+const DESCRIPTION =
+  "A goal-driven and passionate student web developer based in the Philippines, solely focused on creating react applications.";
 
 export const metadata: Metadata = {
   title: TITLE,
@@ -26,14 +28,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en" className="bg-background">
       <body className={jost.className}>
+        {modal}
         <Navbar />
         {children}
+        <Socials />
       </body>
     </html>
   );
