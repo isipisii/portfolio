64319b0f7fe151be34fecdb67479f516c00bc48c
Alessandro Benig <alebenig4@gmail.com>
2025-07-25 15:13:34 +0800

    refactor: space remove

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,6 +1,6 @@
 import { Jost, Just_Me_Again_Down_Here,} from "next/font/google";
  
-export const  just_me_again_down_here = Just_Me_Again_Down_Here({
+export const just_me_again_down_here = Just_Me_Again_Down_Here({
   subsets: ['latin'],
   weight: "400"
 })
