964cc0c6b56bb91c051c8fd46526721233cab2ec
Alessandro Benig <alebenig4@gmail.com>
2025-07-26 14:44:28 +0800

    feat:add separate component of footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,17 @@ import AboutMeSection from "../components/sections/about-me-section";
 import TechnologiesSection from "../components/sections/technologies-section";
 import ProjectsSection from "../components/sections/projects-section";
 import ContactSection from "../components/sections/contact-section";
-import { SiNextdotjs } from "react-icons/si";
+import Footer from "@/components/sections/footer";
 
 export default async function Home() {
-
-  return (
-    <main className="w-full px-8 justify-center flex flex-col gap-10 items-center overflow-hidden">
-      <HeroSection />
-      {/* <AboutMeSection /> */}
-      <TechnologiesSection />
-      <ProjectsSection />
-      <ContactSection />
-      <footer className="w-full">
-        <div className="pt-12 pb-4 flex flex-col gap-8">
-          <div className="text-textMuted/70 text-sm md:text-base grid gap-2 place-items-center">
-            <p> Designed and Built by Alessandro Benig</p>
-            <p>Powered by</p>
-            <SiNextdotjs className="size-8 text-white/80" />
-          </div>
-        </div>
-      </footer>
-    </main>
-  );
+	return (
+		<main className="w-full px-8 justify-center flex flex-col gap-10 items-center overflow-hidden">
+			<HeroSection />
+			{/* <AboutMeSection /> */}
+			<TechnologiesSection />
+			<ProjectsSection />
+			<ContactSection />
+			<Footer />
+		</main>
+	);
 }
--- src/components/layout/socials.tsx
+++ src/components/layout/socials.tsx
@@ -4,26 +4,26 @@ import { motion } from "framer-motion";
 import { SiFacebook, SiGithub, SiLinkedin } from "react-icons/si";
 
 export default function Socials() {
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.2, delay: 1 }}
-      className="hidden fixed bottom-0 right-12 lg:grid gap-8 place-items-center"
-    >
-      <div className="grid gap-8">
-        <a href="https://github.com/isipisii" target="_blank">
-          <SiGithub className="text-headerText size-6" />
-        </a>
-        <a href="https://web.facebook.com/alessandro.benig" target="_blank">
-          <SiFacebook className="text-headerText size-6" />
-        </a>
-          <a href="https://www.linkedin.com/in/alessandro-benig-3b8b7a257/" target="_blank">
-          <SiLinkedin className="text-headerText size-6" />
-        </a>
-      </div>
-      <span className="w-[1px] bg-headerText h-[100px]"></span>
-    </motion.div>
-  );
+	return (
+		<motion.div
+			initial={{ opacity: 0 }}
+			whileInView={{ opacity: 1 }}
+			viewport={{ once: true }}
+			transition={{ duration: 0.2, delay: 1 }}
+			className="hidden fixed bottom-0 right-12 lg:grid gap-8 place-items-center"
+		>
+			<div className="grid gap-8">
+				<a href="https://github.com/isipisii" target="_blank">
+					<SiGithub className="text-headerText size-6" />
+				</a>
+				<a href="https://web.facebook.com/alessandro.benig" target="_blank">
+					<SiFacebook className="text-headerText size-6" />
+				</a>
+				<a href="https://www.linkedin.com/in/alessandro-benig-3b8b7a257/" target="_blank">
+					<SiLinkedin className="text-headerText size-6" />
+				</a>
+			</div>
+			<span className="w-[1px] bg-headerText h-[100px]"></span>
+		</motion.div>
+	);
 }
